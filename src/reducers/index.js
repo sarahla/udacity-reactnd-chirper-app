c6f24643a12abff53db8ff952018c6b3362f7fe5
Sarah Larson <sarah.larson@beamland.com>
2020-01-29 22:51:55 -0500

    Adding reducers and middleware

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux'
+
+import authedUser from '../reducers/authedUser'
+import users from '../reducers/users'
+import tweets from '../reducers/tweets'
+
+export default combineReducers({
+    authedUser,
+    users,
+    tweets
+})

c54035556633b379e4c31ef57c52abf554a1057f
Sarah Larson <sarah.larson@beamland.com>
2020-01-30 16:20:06 -0500

    Add like/unlike feature

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,6 +2,7 @@ import React,  { Component } from 'react'
 import { connect } from 'react-redux'
 import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline}  from 'react-icons/ti'
+import { handleToggleTweet } from '../actions/tweets'
 
 class Tweet extends Component {
     
@@ -12,7 +13,14 @@ class Tweet extends Component {
 
     handleLike = (e, id) => {
         e.preventDefault();
-        //todo: like tweet
+        
+        const { dispatch, tweet, authedUser } = this.props
+        dispatch(handleToggleTweet({
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser,
+        }))
+
     }
 
     render() {

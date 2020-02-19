419ba3e89ce9406afab0d3dc49f829e8d3c12f28
Sarah Larson <sarah.larson@beamland.com>
2020-02-18 22:06:32 -0500

    Adding routes

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -3,12 +3,13 @@ import { connect } from 'react-redux'
 import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline}  from 'react-icons/ti'
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
     
     toParent = (e, id) => {
         e.preventDefault();
-        //todo: redirect to parent tweet
+        this.props.history.push(`/tweet/${id}`)
     }
 
     handleLike = (e, id) => {
@@ -38,11 +39,12 @@ class Tweet extends Component {
             hasLiked, 
             likes, 
             replies, 
-            parent
+            parent,
+            id
         } = tweet
 
         return (
-            <div className="tweet">
+            <Link to={`/tweet/${id}`} className="tweet">
                 <img 
                     src={avatar}
                     alt={`Avatar of ${name}`}
@@ -71,7 +73,7 @@ class Tweet extends Component {
                         <span>{likes !==0 && likes}</span>
                     </div>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
@@ -86,4 +88,4 @@ function mapStateToProps({authedUser, users, tweets}, {id}) {
     }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
